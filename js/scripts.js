c93b33dff13caab71ceed6845f953b33e3a74b92
coolmichaelfritz <fritz.michael11@gmail.com>
2018-03-11 01:03:03 -0800

    add logic to display user results

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     var userNumber = parseInt($("input#ping").val())
+    var results = []
     for (var index = 0, index <= userNumber, index ++) {
-      if (userNumber % 3 === 0) {
-        ping
-      }
-      else if (userNumber % 5 === 0) {
-        pong
-      }
-      elst if (userNumber % 15 === 0) {
-        ping-pong
-      }
-    }
+      results.push(index);
+      // if (userNumber % 3 === 0) {
+      //   ping
+      // }
+      // else if (userNumber % 5 === 0) {
+      //   pong
+      // }
+      // elst if (userNumber % 15 === 0) {
+      //   ping-pong
+      // }
+    };
 
     $("#letsCount").show();
 
