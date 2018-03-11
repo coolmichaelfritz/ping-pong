dc2447a6f13a949a3b595859e88e3253f5d6e08c
coolmichaelfritz <fritz.michael11@gmail.com>
2018-03-10 21:19:05 -0800

    add javascript for event submit preventDefault, and UI logic for parseInt-ing the user's input

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("form#ping-pong").submit(function(event) {
+    var userNumber = parseInt($("input#ping").val())
+
+
+   $("#letsCount").show();
+
+
+
+
+
+  event.preventDefault();
+  });
+
+});
